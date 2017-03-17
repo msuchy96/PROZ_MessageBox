1d9b82b760270114dfbf2ac7ff77b06ee64b6ced
Maciej Suchocki <m.suchocki96@gmail.com>
2017-03-17 17:45:10 +0100

    Added MessageBoxTests

---
+++ src/application/MessageBoxTests.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+package application;
+import java.util.Scanner;
+
+public class MessageBoxTests {
+	/**
+	 * 
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		String title;
+		String information;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Give Title:");
+		title = input.nextLine();
+		
+		System.out.println("Give Information: ");
+		information = input.nextLine();
+		
+		MessageBoxResult result = MessageBox.show(title,information,MessageBoxButtons.YesNoCancel,MessageBoxIcons.Alert,args);
+		
+		System.out.println("Odpowiedü: " + result);
+		
+		System.exit(0);
+	}
+}
